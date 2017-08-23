7f55d819080f55b0797a1bcb0e9ae4474e6bd131
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2017-08-23 17:29:55 -0500

    Web

--- Desarrollo Web/Primeros pasos/index.php
+++ Desarrollo Web/Primeros pasos/index.php
@@ -1,25 +1,36 @@
 <?php
-$titulo="Pagina";
+$titulo="Android";
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title><?php echo $titulo ?></title>
-	<meta name="description" content="Pagina de demostracion de uso de los meta elementos">
-	<meta name="keywords" content="meta, php, poo">
+	<meta name="description" content="Android, el sistema operativo que esta dando de que hablar en la comunidad de programadores por sus comodas herramientas de desarrollo">
+	<meta name="keywords" content="Android, desarrollo, herramientas">
 	<meta name="author" content="Luis Enrique">
-	<meta name="copyright" content="No hay">
+	<meta name="copyright" content="lesh">
 	 <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Viewport para dispositivos moviles -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-<header id="header" class="">
-	<h1>Hola mundo desde PHP</h1>
-	<h2>Estamos en clase</h2>
+<header id="" class="">
+	<!--<img src="img/android.png" style="float: left; width: 100px; height:100px;">--><h1>Android</h1>
 </header><!-- /Termina la cabecera -->
-<body>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio inventore recusandae enim vel est quidem magnam ex earum ratione facilis modi, possimus minima quisquam ab unde, autem quod. Sit, earum!</p>
+<body ><!--Comienzo del cuerpo-->
+	<!--<h2 style="clear: both; text-align: center" >--><h2>¿Que es android?</h2>
+	<p>Android es un sistema operativo basado en el núcleo Linux. Fue diseñado principalmente para dispositivos móviles con pantalla táctil, como teléfonos inteligentes, tabletas y también para relojes inteligentes, televisores y automóviles. Inicialmente fue desarrollado por Android Inc., empresa que Google respaldó económicamente y más tarde, en 2005, compró. Android fue presentado en 2007 junto la fundación del Open Handset Alliance (un consorcio de compañías de hardware, software y telecomunicaciones) para avanzar en los estándares abiertos de los dispositivos móviles. El primer móvil con el sistema operativo Android fue el HTC Dream y se vendió en octubre de 2008. Los dispositivos de Android venden más que las ventas combinadas de Windows Phone e IOS.</p>
+	<h2>Herramientas de desarrollo</h2>
+	<h4>Android Studio</h4>
+	<p>Android Studio es el entorno de desarrollo integrado oficial para la plataforma Android. Fue anunciado el 16 de mayo de 2013 en la conferencia Google I/O, y reemplazó a Eclipse como el IDE oficial para el desarrollo de aplicaciones para Android. La primera versión estable fue publicada en diciembre de 2014.
+
+Está basado en el software IntelliJ IDEA de JetBrains y ha sido publicado de forma gratuita a través de la Licencia Apache 2.0. Está disponible para las plataformas Microsoft Windows, macOS y GNU/Linux. Ha sido diseñado específicamente para el desarrollo de Android.
+
+Estuvo en etapa de vista previa de acceso temprano a partir de la versión 0.1, en mayo de 2013, y luego entró en etapa beta a partir de la versión 0.8, lanzada en junio de 2014. La primera compilación estable, la versión 1.0, fue lanzada en diciembre de 2014.</p>
+<h4>Mit App Inventor</h4>
+<p>App Inventor es un entorno de desarrollo de software creado por Google Labs para la elaboración de aplicaciones destinadas al sistema operativo Android. El usuario puede, de forma visual y a partir de un conjunto de herramientas básicas, ir enlazando una serie de bloques para crear la aplicación. El sistema es gratuito y se puede descargar fácilmente de la web. Las aplicaciones creadas con App Inventor están limitadas por su simplicidad, aunque permiten cubrir un gran número de necesidades básicas en un dispositivo móvil.
+
+Con Google App Inventor, se espera un incremento importante en el número de aplicaciones para Android debido a dos grandes factores: la simplicidad de uso, que facilitará la aparición de un gran número de nuevas aplicaciones; y Google Play, el centro de distribución de aplicaciones para Android donde cualquier usuario puede distribuir sus creaciones libremente.</p>
 </body><!--/Termina el cuerpo de la pagina-->
 </html>
