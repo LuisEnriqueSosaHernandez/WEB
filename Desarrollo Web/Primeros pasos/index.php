c743aab6c467467c1d37f922d8c96735b9f3884f
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2017-08-22 21:53:15 -0500

    Desarrollo web

---
+++ Desarrollo Web/Primeros pasos/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Inicio</title>
+</head>
+<header id="header" class="">
+	<h1>Hola mundo desde PHP</h1>
+	<h2>Estamos en clase</h2>
+</header><!-- /Termina la cabecera -->
+<body>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio inventore recusandae enim vel est quidem magnam ex earum ratione facilis modi, possimus minima quisquam ab unde, autem quod. Sit, earum!</p>
+</body><!--/Termina el cuerpo de la pagina-->
+</html>
