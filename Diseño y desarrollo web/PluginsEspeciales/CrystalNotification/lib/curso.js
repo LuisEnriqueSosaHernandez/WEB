52e34a6b2fa266bc81be5a97e9259abd6c2a587d
Luis Enrique Sosa Hernandez <kiquesasuke@gmail.com>
2020-05-09 12:36:25 -0500

    Plugins Crystal and other

---
+++ Diseño y desarrollo web/PluginsEspeciales/CrystalNotification/lib/curso.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	$.CrystalNotification({
+		position: 2, // try 2, 3 and 4
+		title: "Hello!",
+		image: "plugins/crystal/img/Colorfull/Messages-colorfull.png",
+		content: "Ready to go!",
+	});
+	
+});
